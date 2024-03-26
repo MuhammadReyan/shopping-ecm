098160b713a40bf7eab04bf63b33e2296763504a
MuhammadReyan <muhammadreyan275@gmail.com>
2024-03-27 00:53:39 +0500

    update

--- src/Components/FAQ.jsx
+++ src/Components/FAQ.jsx
@@ -3,14 +3,14 @@ import Accordion from "./Accordion";
 
 const FAQ = () => {
     return (
-        <div className="bg-white rounded-lg py-2">
+        <div className="bg-white rounded-lg py-2 px-4">
             <h1 className="text-center text-4xl text-[#00df9a] font-bold py-3 "> FAQs </h1>
             <Accordion
                 title="Do you prefer Android or iOS"
                 answer="I like to use iOS products"
             />
             <Accordion
-                title="Do you prefer writing CSS or Tailwind?"
+                title="Do you prefer writing Tailwind?"
                 answer="I like to use Tailwind"
             />
             <Accordion title="Firebase or Supabase?" answer="I am using Supabase!" />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-[#00df9a] sticky top-0 z-50 bg-white'>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black sticky top-0 z-50 bg-white'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
       <ul className='hidden md:flex'>
         <li className='p-4'>Home</li>
@@ -19,16 +19,22 @@ const Navbar = () => {
         <li className='p-4'>Contact</li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+        {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-gray-900 ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+      {/* b */}
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full bg-white ease-in-out duration-500  shadow-gray-600' : 'ease-in-out duration-500 fixed left-[-100%]'}>
         <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'>Company</li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4'>Contact</li>
+        <li className='p-4 border-b border-gray-600 hover:text-[#00df9a]'>Home</li>
+        <li className='p-4 border-b border-gray-600 hover:text-[#00df9a]'>Company</li>
+        <li className='p-4 border-b border-gray-600 hover:text-[#00df9a]'>Resources</li>
+        <li className='p-4 border-b border-gray-600 hover:text-[#00df9a]'>About</li>
+        <li className='p-4 hover:text-[#00df9a]'>Contact</li>
       </ul>
+      <style jsx>{`
+        .p-4:hover {
+          color: #00df9a !important;
+        }
+      `}</style>
     </div>
   );
 };
